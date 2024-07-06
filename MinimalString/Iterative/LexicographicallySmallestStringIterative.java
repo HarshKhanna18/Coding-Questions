88be9e4c007d6b5a7a24bcc740efc6d3f03dc629
Harsh <harshkhanna208@gmail.com>
2024-07-07 01:50:45 +0530

    Update Comments

--- MinimalString/Iterative/LexicographicallySmallestStringIterative.java
+++ MinimalString/Iterative/LexicographicallySmallestStringIterative.java
@@ -1,8 +1,14 @@
 package MinimalString.Iterative;
+/*
+ * Given a string, find the lexicographically smallest string that can be
+ * obtained by
+ * removing one character.
+ */
 
+//Code by FlareStudy
 public class LexicographicallySmallestStringIterative {
     public static String smallestStringByIteration(String s) {
-        int n = s.length();
+        int n = s.length();// Code by FlareStudy
         for (int i = 0; i < n - 1; i++) {
             if (s.charAt(i) > s.charAt(i + 1)) {
                 return s.substring(0, i) + s.substring(i + 1);
@@ -11,6 +17,7 @@ public class LexicographicallySmallestStringIterative {
         return s.substring(0, n - 1);
     }
 
+    // Code by FlareStudy
     public static void main(String[] args) {
         String input = "acdb";
         System.out.println("Lexicographically Smallest String (Iteration): " + smallestStringByIteration(input));
